530724c31ea48696f46e48dad7eb9cd783332dce
tanisha.priya007@gmail.com <tanisha.priya007@gmail.com>
2023-01-06 21:19:51 +0530

    final submit

--- assets/index.js
+++ assets/index.js
@@ -1,16 +1,16 @@
-let previewContainer= document.getElementById('bookPreview')
-let previewBox= document.getElementById('preview')
 
-document.getElementById('bookSellerContainer book').forEach(book => {
-    book.onclick= () => {
-        previewContainer.style.display= 'flex'
-        let name= book.getAttribute('data-name');
 
-    previewBox.forEach(preview =>{
-        let target=  preview.getAttribute('data-target');
-        if (name === target){
-            preview.classList.add('active')
-        }
-    })
-};
-});
+let popupEl = document.getElementById('popup')
+
+
+function buyBtn() {
+    popupEl.classList.add('open-popup');
+    console.log('buyBtn')
+
+}
+
+function closeSubmit() {
+
+    popupEl.classList.remove('open-popup');
+    console.log(closeSubmit)
+}
